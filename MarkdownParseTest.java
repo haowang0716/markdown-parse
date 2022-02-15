c65534531c73bb6c251f1821698497e6f945f167
Benjamin Xia <benjx32@gmail.com>
2022-02-14 23:20:43 -0800

    rename

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -6,7 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
-public class MarkdownTest {
+public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
